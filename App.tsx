c2e1b8df77dfe7d414b7d59256826eac132992c3
DickySosa <127916303+DickySosa@users.noreply.github.com>
2023-04-24 15:01:20 -0600

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import './style.css';
-import List from './components/List'
+//import List from './components/List'
 
 export default function App() {
   return (
     <div>
-      <List/>
+      <List />
     </div>
   );
 }
